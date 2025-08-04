23b73c95183c7d9f4fd7013aa4a3caca691c5e68
CherinJeni <jenifer.r@officeinfosystems.com>
2025-08-04 15:27:08 +0530

    routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+
+  import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { UserDashboardComponent } from './app/dasboard/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './app/dasboard/admin-dashboard/admin-dashboard.component';
+
+const routes: Routes = [
+  { path: 'user', component: UserDashboardComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  { path: '**', redirectTo: 'user' },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+})
