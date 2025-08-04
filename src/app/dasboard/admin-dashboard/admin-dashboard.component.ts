9deebdad41d8dd212b81026ad5592001ff395c8d
Nachiyappan Subramani <nachiyappan.s@officeinfosystems.com>
2025-08-04 15:16:32 +0530

    Html

--- src/app/dasboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/dasboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+  styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
-
+  users = [
+    {
+      name: 'John Doe',
+      email: 'john@example.com',
+      role: 'User',
+      rating: 4
+    },
+    {
+      name: 'Jane Smith',
+      email: 'jane@example.com',
+      role: 'Admin',
+      rating: 5
+    },
+    {
+      name: 'Alex Brown',
+      email: 'alex@example.com',
+      role: 'Moderator',
+      rating: 3
+    }
+  ];
 }
