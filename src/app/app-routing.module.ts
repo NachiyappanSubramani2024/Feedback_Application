4d91399238f9caefdcb611e683bbdd9b84fb002f
CherinJeni <jenifer.r@officeinfosystems.com>
2025-08-04 15:12:07 +0530

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserDashboardComponent } from './dasboard/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './dasboard/admin-dashboard/admin-dashboard.component';
+
+
+const routes: Routes = [
+  { path: 'user', component: UserDashboardComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  { path: '**', redirectTo: 'user' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
